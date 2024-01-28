c147a762b20e04debb65544e78c62295f6d8bb92
MicReson <92884357+MicReson@users.noreply.github.com>
2024-01-28 18:24:59 -0500

    Update from stereo to mono. Now is possible to pass multiple files as arguments of the script

--- FromStereoToMono.bat
+++ FromStereoToMono.bat
@@ -1,12 +1,20 @@
 @echo off
-SET input=%~1
+
+:Loop
 SET output=%~dpn1-Mono.wav
 
 if "%~1"=="" (
-    echo Usage: .\FromStereoToMono.bat [input_file.wav]
-    exit /b
+    echo Usage: .\FromStereoToMono.bat [Channel_Number] [input_file.wav]
+    goto continue
 )
 
-ffmpeg -i "%input%" -ac 1 "%output%"
+ffmpeg -i %1 -ac 1 "%output%"
+
 echo Conversion complete: %output%
 
+shift
+
+goto Loop
+
+:continue
+exit /b
