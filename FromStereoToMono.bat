b98af540b2ae6114c43dccb605fb1ca8ad135086
MicReson <92884357+MicReson@users.noreply.github.com>
2024-01-28 21:31:38 -0500

    Update script to have option of summing the channels or choosing between left and right channels

--- FromStereoToMono.bat
+++ FromStereoToMono.bat
@@ -1,6 +1,23 @@
 @echo off
 
+SET /P ChooseOption= "Choose 1 if want to add channels and 2 if you want to choose the channels= "
+
+if %ChooseOption%==1 (
+    goto ChannelConvertion
+)
+
+if %ChooseOption%==2 (
+    goto ChannelChoose
+)
+
+if %ChooseOption% NEQ 1 if %ChooseOption% NEQ 2 (
+    echo Not a valid option
+    exit /b
+)
+
+:ChannelConvertion
 :Loop
+
 SET output=%~dpn1-Mono.wav
 
 if "%~1"=="" (
@@ -18,3 +35,33 @@ goto Loop
 
 :continue
 exit /b
+
+:ChannelChoose
+
+SET /P ChooseChannel= "0 is the left channel and 1 is the right channel= "
+if %ChooseChannel% NEQ 0 if %ChooseChannel% NEQ 1 (
+    echo Please choose the correct channel
+    exit /b
+)
+
+:LoopSecondOption
+
+SET UserChannel=%ChooseChannel%
+SET output=%~dpn1-Mono.wav
+
+if "%~1"=="" (
+    echo Usage: .\FromStereoToMono.bat [input_file.wav]
+    goto continueSecondOption
+)
+
+ffmpeg -i %1 -map_channel 0.0.%UserChannel% "%output%"
+
+echo Conversion complete: %output%
+
+shift
+
+goto LoopSecondOption
+
+:continueSecondOption
+exit /b
+
