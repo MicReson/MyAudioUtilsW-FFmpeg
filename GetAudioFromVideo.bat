b5895a256bbf75d00629d88b4f063eca5a890392
MicReson <92884357+MicReson@users.noreply.github.com>
2024-02-02 18:18:58 -0500

    Give audio codec options to get audio from video script

--- GetAudioFromVideo.bat
+++ GetAudioFromVideo.bat
@@ -1,15 +1,29 @@
 @echo off
+SET /P CodecType= "Which codec? 1 for MP3 and 2 for WAV: "
 
+if %CodecType% NEQ 1 if %CodecType% NEQ 2 (
+    echo Please provide a codec option
+    exit /b
+)
+
+if %CodecType%==1 (
+    goto Mp3Codec
+)
+
+if %CodecType%==2 (
+    goto WavCodec
+)
+
+:Mp3Codec
 :Loop
-
 SET output=%~dpn1-AUDIO.mp3
 
 if "%~1"=="" (
-    echo Usage: .\FromStereoToMono.bat [input_file.wav]
+    echo Usage: .\GetAudioFromVideo.bat [input_file.wav]
     goto continue
 )
 
-ffmpeg -i %1 -map 0:a "%output%"
+ffmpeg -i %1 -vn -acodec libmp3lame "%output%"
 
 echo Conversion complete: %output%
 
@@ -19,3 +33,24 @@ goto Loop
 
 :continue
 exit /b
+
+:WavCodec
+:LoopWav
+SET output=%~dpn1-AUDIO.wav
+
+if "%~1"=="" (
+    echo Usage: .\GetAudioFromVideo.bat [input_file.wav]
+    goto continueWav
+)
+
+ffmpeg -i %1 -vn -acodec pcm_s16le "%output%"
+
+echo Conversion complete: %output%
+
+shift
+
+goto LoopWav
+
+:continueWav
+exit /b
+
